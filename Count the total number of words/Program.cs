a35b49456b775063a14fb5a9b8efa5bdb1ca5403
Team Work <63493639+teamwork786@users.noreply.github.com>
2020-04-11 21:56:10 +0500

    Count the total no of words

---
+++ Count the total number of words/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_total_number_of_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words ;
+            int word = 0;
+            Console.Write("Input Words : ");
+            words = Console.ReadLine();
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (words[j] == ' ')
+                {
+                    word++;
+                }
+            }
+            Console.WriteLine("Total Words Are : " + ++word);
+        }
+    }
+}
